61db90d9965ba5117f2964e1b14c90b77762da9b
Christopher House <chhouse@microsoft.com>
2021-02-06 16:46:23 -0600

    added parameters for script

--- run-pipeline.ps1
+++ run-pipeline.ps1
@@ -1,1 +1,18 @@
-#
+[cmdletbinding()]
+param(
+    [parameter(mandatory="true")]
+    [string] $ClientId,
+    [parameter(mandatory="true")]
+    [string] $ClientSecret,
+    [parameter(mandatory="true")]
+    [string] $TenantId,
+    [parameter(mandatory="true")]
+    [string] $PipelineName,
+    [parameter(mandatory="true")]
+    [string] $WorkspaceName,
+    [parameter(mandatory="true")]
+    [string] $ResourceGroupName
+)
+
+# Login with the service principal
+az login --service-principal --username $ClientId --password $ClientSecret --tenant $TenantId
